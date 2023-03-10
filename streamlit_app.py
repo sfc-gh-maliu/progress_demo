b7fbf4be06364e3aa8efe338d6e59941ed83b14e
Maggie Liu <121910051+sfc-gh-maliu@users.noreply.github.com>
2023-03-10 09:45:09 -0800

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,15 @@ import time
 for i in range(10):
     st.write("")
 
-progress_text = "Hang tight"
+progress_text = "Hang tight😬"
 my_bar = st.progress(0, text=progress_text)
 
-for percent_complete in range(100):
-    time.sleep(0.1)
-    if percent_complete == 50:
-        progress_text = "Almost there"
-    if percent_complete == 99:
-        progress_text = "Done!"
-    my_bar.progress(percent_complete + 1, text=progress_text)
+col1, col2 = st.columns([1,1])
+with col1:
+    for percent_complete in range(100):
+        time.sleep(0.1)
+        if percent_complete == 50:
+            progress_text = "Almost there🥹"
+        if percent_complete == 98:
+            progress_text = "Done🥰"
+        my_bar.progress(percent_complete + 1, text=progress_text)
