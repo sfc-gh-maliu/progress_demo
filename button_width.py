974aee4f0ed9f894bb6c1155f281c5646d68140f
Maggie Liu <121910051+sfc-gh-maliu@users.noreply.github.com>
2023-03-09 16:42:34 -0800

    add button

---
+++ button_width.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+import numpy as np
+
+
+
+
+st.button(label = "Button")
+
+
+with st.container():
+    st.button(label = "Make your button as the container width ", use_container_width = True)
+
